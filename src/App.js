02a88f810b7376d9f5ce750d8d298a680f0cd165
April Murillo <afreyde@wgu.edu>
2023-01-09 16:20:15 -0800

    completely working just needs css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Header from './Header';
 import MemeBuilder from './memeBuilder';
+import SavedMeme from './SavedMeme';
+import { v4 as uuidv4 } from 'uuid';
 
-function App() {
+
+function App(props) {
+
+  const [memeList, setMemeList] = useState([])
+  
+      const updateMemeItem = (id, updatedItem) => { 
+        setMemeList(prevMemeList => { 
+            return prevMemeList.map((item)=> { 
+                if (item.id === id) { 
+                    return updatedItem
+                }
+                else {
+                    return item
+                }
+            })
+        })
+    }
+  
+    const addNewItem = (newItem) => {
+      newItem.id = uuidv4()
+      setMemeList(prev=>[...prev, newItem])
+    }
+  
+    function deleteMemeImage(id) {
+      console.log(id)
+      setMemeList(memeList.filter((meme) => meme.id !== id))
+    }
+  
+    const savedMemes = memeList.map((meme, index) => {
+      return  <SavedMeme
+        key={meme.id}
+        topText={meme.topText}
+        bottomText={meme.bottomText}
+        randomImage={meme.randomImage}
+        updateMemeItem={updateMemeItem}
+        deleteMemeImage={deleteMemeImage}
+        id={meme.id}
+      />})
 
   return (
     <div>
       <Header/>
-     <MemeBuilder />
+     <MemeBuilder submit={addNewItem} />
+     <hr/><br></br>
+      <h2 className="saved-memes">Saved Memes List</h2>
+      {savedMemes}
     </div>
   )
 }
--- src/SavedMeme.js
+++ src/SavedMeme.js
@@ -1,69 +1,73 @@
-import React from "react";
+import React, { useState} from "react";
 
-function SavedMeme(){
-class SavedMeme extends React.Component {
 
-  btnDelete = (event) => {
-    event.preventDefault()
-    this.props.btnDelete(this.props.item.id)
+
+export default function SavedMeme(props) {
+
+  const [showForm, setShowForm] = useState(false)
+
+  const [meme, setMeme] = useState({ 
+      topText: props.topText,
+      bottomText: props.bottomText,
+      randomImage:props.randomImage,
+      id:props.id
+  })
+
+  const toggleShowForm = () => { 
+      setShowForm(prev=>!prev)
   }
 
-  btnEdit = (event) => {
-    event.preventDefault()
-    this.props.btnEdit(this.props.item.id)
-  }
-  // Updates TextAreas
-  textEditTop = (event) => {
-    event.preventDefault()
-    this.props.textEditTop(
-      this.props.item.id,
-      event.target.value)
+  const handleChange = (e) => { 
+      const {name, value} = e.target
+      setMeme(prevState=>{
+          return ({
+              ...prevState,
+              [name]:value
+          })
+      })
   }
 
-  textEditBottom = (event) => {
-    event.preventDefault()
-    this.props.textEditBottom(
-      this.props.item.id,
-      event.target.value)
-  }
-
-  render(
-    return (
-      <form className="savedMeme">
-        <textarea
-          name="topText"
-          type="text"
-          disabled={this.props.item.textDisabled}
-          onChange={this.textEditTop}
-          value={this.props.item.topText}
-        />
-        <img
-          src={this.props.item.url}
-          alt={this.props.item.name} />
-        <textarea
-          name="bottomText"
-          type="text"
-          disabled={this.props.item.textDisabled}
-          onChange={this.textEditBottom}
-          value={this.props.item.bottomText}
-        />
-        <div className="savedMemesBtn">
-          <button
-            onClick={this.btnEdit}>
-            {this.props.item.btnTextChange}</button>
-          <button
-            onClick={this.btnDelete}>
-            Delete
-          </button>
-        </div>
-      </form>
-    )
-  )
+  const handleSubmit = (e) => { 
+      e.preventDefault()
+      props.updateMemeItem(props.id, meme)
+      toggleShowForm()
   }
 
 
-
-
-
-
-export default SavedMeme;
+  return (
+      <div className="meme--list">
+          
+          {showForm ? 
+              <form className="edit-form" onSubmit={handleSubmit}>
+                  <input 
+                      name="topText"
+                      placeholder="Top Text"
+                      className="top-text-input"
+                      value={meme.topText}
+                      onChange={handleChange}
+                  />
+                  <input 
+                      name="bottomText"
+                      placeholder="Bottom Text"
+                      className="bottom-text-input"
+                      value={meme.bottomText}
+                      onChange={handleChange}
+                  />
+                  <img src={meme.randomImage} className="meme--image" alt=""/>
+                  <button>Submit</button>
+              </form>
+          :
+          <>
+              <div className="meme-container">
+                  <img src={meme.randomImage} className="meme--image" alt="" />
+                  <h2 className="meme-text top"> {meme.topText} </h2>{" "}
+                  <h2 className="meme-text bottom"> {meme.bottomText} </h2>{" "}
+              </div>{" "}
+          </>
+          }
+          <button onClick={toggleShowForm}>{showForm ? "Close" : "Edit"}</button>
+          <br></br>
+          <button onClick={()=> {props.deleteMemeImage(props.id)}}>Delete</button>
+      </div>
+  );
+}
